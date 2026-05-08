713406be6f0633b561cbc630c8427de605b7dc61
ZhelnovachevRoman <zhelnovachev@gmail.com>
2026-05-08 15:12:41 +0300

    Дописал JavaDoc

--- src/main/java/EffectiveMobile/notification_service/dto/RegisterEvent.java
+++ src/main/java/EffectiveMobile/notification_service/dto/RegisterEvent.java
@@ -2,6 +2,14 @@ package EffectiveMobile.notification_service.dto;
 
 import java.time.Instant;
 
+/**
+ * Событие для кафки о регистрации нового пользователя в системе
+ * @param eventId
+ * @param email
+ * @param code
+ * @param sendTime
+ * @author ZhelnovachevRoman
+ */
 public record RegisterEvent(
         String eventId,
         String email,
--- src/main/java/EffectiveMobile/notification_service/service/NotificationService.java
+++ src/main/java/EffectiveMobile/notification_service/service/NotificationService.java
@@ -3,10 +3,21 @@ package EffectiveMobile.notification_service.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+/**
+ * Сервис отправки кода подтверждения на почту пользователя (в настоящее время выводит логи в консоль)
+ *
+ * @author ZhelnovachevRoman
+ */
 @Service
 @Slf4j
 public class NotificationService {
 
+    /**
+     * Метод отправки кода подтверждения на почту пользователя
+     * В настоящее время просто выводит логи в консоль
+     * @param email
+     * @param code
+     */
     public void send(String email, String code){
         log.info("=== Отправка кода подтверждения ===");
         log.info("Email: {}", email);
